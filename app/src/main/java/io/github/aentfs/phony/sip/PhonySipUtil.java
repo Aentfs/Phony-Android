d0234baded1442d765bc8797072cc75124718c91
Stefan S <aentfs@aentfs.de>
2017-06-25 15:19:36 +0200

    Add SIP methods to code

---
+++ app/src/main/java/io/github/aentfs/phony/sip/PhonySipUtil.java
@@ -0,0 +1,40 @@
+package io.github.aentfs.phony.sip;
+
+import android.content.Context;
+import android.net.sip.SipManager;
+import android.net.sip.SipProfile;
+
+import java.text.ParseException;
+
+/**
+ * Utility class for interacting with the SIP system.
+ */
+public class PhonySipUtil {
+
+    private PhonySipUtil() {//empty
+    }
+
+    private static SipManager mSipManager = null;
+
+    /**
+     * Get the sip account manager for the current context.
+     *
+     * @param context The current context to use.
+     * @return The {@link SipManager} for the given context.
+     */
+    public static SipManager getSipManager(Context context) {
+        if (mSipManager == null) {
+            mSipManager = SipManager.newInstance(context);
+        }
+
+        return mSipManager;
+    }
+
+    public static SipProfile createSipProfile(String username, String password, String domain, String proxyAddress) throws ParseException {
+        SipProfile.Builder builder = new SipProfile.Builder(username, domain);
+        builder.setPassword(password)
+                .setOutboundProxy(proxyAddress);
+
+        return builder.build();
+    }
+}
